f9f1eed6ed9c44b17cadd9c048ef54eee405ccc5
gitsyskey6332 <109967218+gitsyskey6332@users.noreply.github.com>
2022-07-25 18:41:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests, random, string, time, os
 
-token = os.environ.get("BOT_TOKEN")
-chatid = os.environ.get("FORWARD_ID")
+token = os.environ.get("5416939382:AAHAtSLxvsh9_DAwjlJIofAMoV2pi5_zeYE")
+chatid = os.environ.get("-787266910")
 
 def long_key():
   skkey = random.choice(['sk_live_51H', 'sk_live_51J'])+''.join(random.choices( string.digits + string.ascii_letters, k = 96))
